195de8f1605427c7a922d9c928952bcec301cbae
James Nesbitt <james.nesbitt@wunderkraut.com>
2016-09-08 10:32:54 +0300

    removed broken local handler related architecture

--- api/handler/local/local.go
+++ api/handler/local/local.go
@@ -5,17 +5,3 @@ package local
  * based on current local path, and the configuration files contained
  * therein (and perhaps also in the user home folder somewhere)
  */
-
-import (
-	"github.com/james-nesbitt/wundertools-go/api"
-	"github.com/james-nesbitt/wundertools-go/api/handler"
-)
-
-// Api Constructor for which uses only a Local Handler
-func MakeLocalAPI(PathProject string) api.API {
-	local := api.BaseAPI{}
-
-	local.AddHandler(handler.Handler(NewLocalHandler(PathProject)))
-
-	return api.API(&local)
-}
