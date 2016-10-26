5535549c89f0cbdccfb108ae990a5b33bb01aa6c
Aleksi Johansson <aleksi.johansson@wunderkraut.com>
2016-10-26 22:07:37 +0300

    Added detection of some host operating systems to automatically set the build target.

--- build.sh
+++ build.sh
@@ -13,17 +13,44 @@
 
 set -e
 
+# Get OS and architecture so that we can build correct binary for the host.
+OS="$(uname | tr 'A-Z' 'a-z')"
+ARC="$(uname -m)"
+
+# Convert $ARC to accepted $GOARCH values by hand. Only know cases work.
+# See list of options here https://golang.org/doc/install/source.
+if [ $ARC == "x86_64" ]
+then
+  ARC="amd64"
+elif [ "$ARC" == "ARMv7" ] || [ "$ARC" == "ARMv6" ]
+then
+  ARC="arm"
+elif [ "$ARC" == "ARMv8" ]
+then
+  ARC="arm64"
+fi
+
+# Only set $GOOS and $GOARCH if they are not set to allow overriding the build target.
+if [ -z "$GOOS" ]
+then
+  GOOS=${OS}
+fi
+if [ -z "$GOARCH" ]
+then
+  GOARCH=${ARC}
+fi
+
 source build/make.sh
 
 EXEC_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 INTERNAL_LIBRARY_PATH="github.com/james-nesbitt/wundertools-go"
 
-echo "This will build wundertools as a 'kraut' for '$GOOS-$GOARCH' (you can change that)
+echo "This will build wundertools as a 'kraut' for '$GOOS-$GOARCH'. Change this by setting \$GOOS and \$GOARCH environment variables.
 
  **** Building Wundertools in containerized golang environment
  "
 
-docker run --rm -ti -v "${EXEC_PATH}:/usr/src/myapp" -v "${EXEC_PATH}:/go/src/${INTERNAL_LIBRARY_PATH}" -e GOOS -e GOARCH -w /usr/src/myapp golang:${GOVERSION} make getdeps build
+docker run --rm -ti -v "${EXEC_PATH}:/usr/src/myapp" -v "${EXEC_PATH}:/go/src/${INTERNAL_LIBRARY_PATH}" -e "GOOS=${GOOS}" -e "GOARCH=${GOARCH}" -w /usr/src/myapp golang:${GOVERSION} make getdeps build
 
 echo " **** Containerized build complete
 
