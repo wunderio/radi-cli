8cfd19399a7d7c3573012ed56c40fca5a9949096
James Nesbitt <james.nesbitt@wunderkraut.com>
2016-11-15 13:44:44 +0200

    initial upcloud operational support

--- local/build.go
+++ local/build.go
@@ -6,6 +6,7 @@ import (
 	api_builder "github.com/james-nesbitt/kraut-api/builder"
 	api_config "github.com/james-nesbitt/kraut-api/operation/config"
 	handlers_configconnect "github.com/james-nesbitt/kraut-handlers/configconnect"
+	handlers_upcloud "github.com/james-nesbitt/kraut-handlers/upcloud"
 )
 
 func LocalBuild(localApi *api_builder.BuilderAPI) {
@@ -28,8 +29,15 @@ func LocalBuild(localApi *api_builder.BuilderAPI) {
 
 	} else {
 
+		built := BuildTracker{}
+		built.Add("local")
+
 		for _, key := range builderList {
 			builderSetting, _ := builderConfigWrapper.Get(key)
+
+			if !built.IsBuilt(builderSetting.Type) {
+				localAddBuilder(localApi, builderSetting.Type)
+			}
 
 			log.WithFields(log.Fields{"type": builderSetting.Type, "implementations": builderSetting.Implementations.Order(), "key": key}).Debug("Activate builder from settings")
 
@@ -38,4 +46,36 @@ func LocalBuild(localApi *api_builder.BuilderAPI) {
 
 	}
 
+}
+
+// Add builders to the BuilderAPI, considering that this is a local app
+func localAddBuilder(localApi *api_builder.BuilderAPI, key string) {
+	switch key {
+	case "local":
+		// ignore me
+	case "upcloud":
+		log.Debug("LocalAPI: Building UpCloud builder")
+		localApi.AddBuilder(api_builder.Builder(&handlers_upcloud.UpcloudBuilder{}))
+	default:
+		log.WithFields(log.Fields{"builder": key}).Error("Unknown builder referenced in local project")
+	}
+}
+
+// Simple struct tracks which builders have already been added
+type BuildTracker struct {
+	built []string
+}
+
+func (tracker *BuildTracker) Add(add string) {
+	if !tracker.IsBuilt(add) {
+		tracker.built = append(tracker.built, add)
+	}
+}
+func (tracker *BuildTracker) IsBuilt(add string) bool {
+	for _, built := range tracker.built {
+		if add == built {
+			return true
+		}
+	}
+	return false
 }
