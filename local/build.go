d1d50d4b126c1b8b18aed93114ce962bd694288f
James Nesbitt <james.nesbitt@wunderkraut.com>
2016-11-16 16:42:16 +0200

    remove some unneeded architecture in the builder

--- local/build.go
+++ local/build.go
@@ -1,6 +1,8 @@
 package local
 
 import (
+	"errors"
+
 	log "github.com/Sirupsen/logrus"
 
 	api_builder "github.com/james-nesbitt/kraut-api/builder"
@@ -9,6 +11,12 @@ import (
 	handlers_upcloud "github.com/james-nesbitt/kraut-handlers/upcloud"
 )
 
+// Take a LocalAPI, and add builders to it based on what can be found from the config operations
+/**
+ * This function uses an APIs existing config operations, to act as a source of build information
+ * which is used to add builders to the API.  The build process tries to identify builders based
+ * on the config Type: information,  through simple string matching.
+ */
 func LocalBuild(localApi *api_builder.BuilderAPI) {
 
 	// build at least the config operations, which we will need for a config wrapper
@@ -29,27 +37,38 @@ func LocalBuild(localApi *api_builder.BuilderAPI) {
 
 	} else {
 
-		built := BuildTracker{}
-		built.Add("local")
+		built := map[string]bool{}
+		built["local"] = true
 
 		for _, key := range builderList {
 			builderSetting, _ := builderConfigWrapper.Get(key)
 
-			if !built.IsBuilt(builderSetting.Type) {
-				localAddBuilder(localApi, builderSetting.Type)
+			if _, checked := built[builderSetting.Type]; !checked {
+				if err := localAddBuilder(localApi, builderSetting.Type); err == nil {
+					built[builderSetting.Type] = true
+				} else {
+					built[builderSetting.Type] = false
+				}
 			}
 
-			log.WithFields(log.Fields{"type": builderSetting.Type, "implementations": builderSetting.Implementations.Order(), "key": key}).Debug("Activate builder from settings")
-
-			localApi.ActivateBuilder(builderSetting.Type, builderSetting.Implementations, builderSetting.SettingsProvider)
+			if built[builderSetting.Type] {
+				log.WithFields(log.Fields{"type": builderSetting.Type, "implementations": builderSetting.Implementations.Order(), "key": key}).Debug("Activate builder from settings")
+				localApi.ActivateBuilder(builderSetting.Type, builderSetting.Implementations, builderSetting.SettingsProvider)
+			} else {				
+				log.WithError(err).WithFields(log.Fields{"builder": builderSetting.Type}).Error("Unknown builder referenced in local project")
+			}
 		}
 
 	}
 
 }
 
-// Add builders to the BuilderAPI, considering that this is a local app
-func localAddBuilder(localApi *api_builder.BuilderAPI, key string) {
+// Add builders to the BuilderAPI, so that they can be activated
+/**
+ * This function needs to be aware of what builders are allowed in thie
+ * localAPI, which means that it is tied to the .go import() items.
+ */
+func localAddBuilder(localApi *api_builder.BuilderAPI, key string) error {
 	switch key {
 	case "local":
 		// ignore me
@@ -57,25 +76,7 @@ func localAddBuilder(localApi *api_builder.BuilderAPI, key string) {
 		log.Debug("LocalAPI: Building UpCloud builder")
 		localApi.AddBuilder(api_builder.Builder(&handlers_upcloud.UpcloudBuilder{}))
 	default:
-		log.WithFields(log.Fields{"builder": key}).Error("Unknown builder referenced in local project")
+		return errors.New("Unrecognized builder " + key)
 	}
-}
-
-// Simple struct tracks which builders have already been added
-type BuildTracker struct {
-	built []string
-}
-
-func (tracker *BuildTracker) Add(add string) {
-	if !tracker.IsBuilt(add) {
-		tracker.built = append(tracker.built, add)
-	}
-}
-func (tracker *BuildTracker) IsBuilt(add string) bool {
-	for _, built := range tracker.built {
-		if add == built {
-			return true
-		}
-	}
-	return false
+	return nil
 }
